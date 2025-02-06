d3640f5265684e290036ab87df42cd59af30d15b
Fabio Henrique <112027773+FabioHenrique2003@users.noreply.github.com>
2025-02-06 11:57:13 -0300

    Update GridAdapter.java

--- app/src/main/java/cozer/ribeiro/fantin/henrique/fabio/galeriapublica/GridAdapter.java
+++ app/src/main/java/cozer/ribeiro/fantin/henrique/fabio/galeriapublica/GridAdapter.java
@@ -14,25 +14,40 @@ import androidx.recyclerview.widget.DiffUtil;
 import java.text.SimpleDateFormat;
 
 public class GridAdapter extends PagingDataAdapter<ImageData, MyViewHolder> {
-    public GridAdapter(@NonNull DiffUtil.ItemCallback<ImageData>
-                               diffCallback) {super(diffCallback);}
+    public GridAdapter(@NonNull DiffUtil.ItemCallback<ImageData> diffCallback) {
+        super(diffCallback);
+    }
+
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Cria um objeto LayoutInflater e infla o layout 'list_item' para cada item da lista.
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        View view = layoutInflater.inflate(R.layout.grid_item, parent, false);
+        View view = layoutInflater.inflate(R.layout.list_item, parent, false);
+        // Retorna uma instância de MyViewHolder associada ao layout criado.
         return new MyViewHolder(view);
     }
+
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        // Obtém a instância de ImageData correspondente à posição atual.
         ImageData imageData = getItem(position);
+
+        // Define o nome do arquivo de imagem no TextView correspondente.
         TextView tvName = holder.itemView.findViewById(R.id.tvName);
         tvName.setText(imageData.fileName);
+
+        // Formata a data no padrão "HH:mm dd/MM/yyyy" e define no TextView apropriado.
         TextView tvDate = holder.itemView.findViewById(R.id.tvDate);
         tvDate.setText("Data: " + new SimpleDateFormat("HH:mm dd/MM/yyyy").format(imageData.date));
+
+        // Exibe o tamanho do arquivo no respectivo TextView.
         TextView tvSize = holder.itemView.findViewById(R.id.tvSize);
         tvSize.setText("Tamanho: " + String.valueOf(imageData.size));
+
+        // Obtém a miniatura (bitmap) armazenada na instância de ImageData.
         Bitmap thumb = imageData.thumb;
+        // Exibe a miniatura no ImageView correspondente.
         ImageView imageView = holder.itemView.findViewById(R.id.imThumb);
         imageView.setImageBitmap(thumb);
     }
