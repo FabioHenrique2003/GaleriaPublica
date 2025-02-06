aa7091a2b6359e12ecdd3d2acfbe3c7610ecb634
Fabio Henrique <112027773+FabioHenrique2003@users.noreply.github.com>
2025-02-06 13:11:21 -0300

    Update ImageDataComparator.java

--- app/src/main/java/cozer/ribeiro/fantin/henrique/fabio/galeriapublica/ImageDataComparator.java
+++ app/src/main/java/cozer/ribeiro/fantin/henrique/fabio/galeriapublica/ImageDataComparator.java
@@ -3,14 +3,20 @@ package cozer.ribeiro.fantin.henrique.fabio.galeriapublica;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DiffUtil;
 
-class ImageDataComparator extends DiffUtil.ItemCallback<ImageData> {
-    @Override
-    public boolean areItemsTheSame(@NonNull ImageData oldItem, @NonNull ImageData newItem) {
-        // Id is unique.
-        return oldItem.uri.equals(newItem.uri);
-    }
-    @Override
-    public boolean areContentsTheSame(@NonNull ImageData oldItem, @NonNull ImageData newItem) {
-        return oldItem.uri.equals(newItem.uri);
-    }
-}
+class ImageDataComparator extends
+        DiffUtil.ItemCallback<ImageData> {
+ @Override
+ public boolean areItemsTheSame(@NonNull ImageData oldItem,
+@NonNull ImageData newItem)
+    {
+         // Id is unique.
+         return oldItem.uri.equals(newItem.uri);
+         }
+ @Override
+ public boolean areContentsTheSame(@NonNull ImageData
+                                              oldItem,
+ @NonNull ImageData
+                                              newItem) {
+         return oldItem.uri.equals(newItem.uri);
+         }
+ }
