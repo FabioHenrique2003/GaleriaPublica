9985fd6406d006cdcbdb22653806f833cd024ecb
Fabio Henrique <112027773+FabioHenrique2003@users.noreply.github.com>
2025-02-06 11:58:58 -0300

    Update GridViewFragment.java

--- app/src/main/java/cozer/ribeiro/fantin/henrique/fabio/galeriapublica/GridViewFragment.java
+++ app/src/main/java/cozer/ribeiro/fantin/henrique/fabio/galeriapublica/GridViewFragment.java
@@ -2,63 +2,66 @@ package cozer.ribeiro.fantin.henrique.fabio.galeriapublica;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.paging.PagingData;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link GridViewFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class GridViewFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private MainViewModel mViewModel;
+    private View view;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public GridViewFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GridViewFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GridViewFragment newInstance(String param1, String param2) {
-        GridViewFragment fragment = new GridViewFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    // Método para criar uma nova instância do fragmento.
+    public static GridViewFragment newInstance() {
+        return new GridViewFragment();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        // Infla o layout "fragment_grid_view" e retorna a View correspondente.
+        view = inflater.inflate(R.layout.fragment_grid_view, container, false);
+        return view;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_grid_view, container, false);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Inicializa o ViewModel a partir da atividade associada.
+        mViewModel = new ViewModelProvider(getActivity()).get(MainViewModel.class);
+
+        // Instancia o adaptador para o RecyclerView.
+        GridAdapter gridAdapter = new GridAdapter(new ImageDataComparator());
+
+        // Obtém o LiveData que contém a lista paginada de ImageData.
+        LiveData<PagingData<ImageData>> liveData = mViewModel.getPageLv();
+
+        // Monitora alterações no LiveData para atualizar o adaptador sempre que os dados mudarem.
+        liveData.observe(getViewLifecycleOwner(), new Observer<PagingData<ImageData>>() {
+            @Override
+            public void onChanged(PagingData<ImageData> objectPagingData) {
+                // Envia os novos dados ao adaptador, respeitando o ciclo de vida do fragmento.
+                gridAdapter.submitData(getViewLifecycleOwner().getLifecycle(), objectPagingData);
+            }
+        });
+
+        // Localiza o RecyclerView no layout e associa o adaptador.
+        RecyclerView rvGallery = (RecyclerView) view.findViewById(R.id.rvGrid);
+        rvGallery.setAdapter(gridAdapter);
+
+        // Configura o layout do RecyclerView como uma lista vertical usando LinearLayoutManager.
+        rvGallery.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 }
