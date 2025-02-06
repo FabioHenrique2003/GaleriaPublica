623a0bc7b02021450b32415e587dc4ad7686957b
Fabio Henrique <112027773+FabioHenrique2003@users.noreply.github.com>
2025-02-06 13:10:32 -0300

    Update ImageData.java

--- app/src/main/java/cozer/ribeiro/fantin/henrique/fabio/galeriapublica/ImageData.java
+++ app/src/main/java/cozer/ribeiro/fantin/henrique/fabio/galeriapublica/ImageData.java
@@ -11,11 +11,12 @@ public class ImageData {
     public String fileName;
     public Date date;
     public int size;
-    public ImageData(Uri uri, Bitmap thumb, String fileName, Date date, int size) {
-        this.uri = uri;
-        this.thumb = thumb;
-        this.fileName = fileName;
-        this.date = date;
-        this.size = size;
-    }
+ public ImageData(Uri uri, Bitmap thumb, String fileName, Date
+            date, int size) {
+ this.uri = uri;
+ this.thumb = thumb;
+ this.fileName = fileName;
+ this.date = date;
+ this.size = size;
+ }
 }
